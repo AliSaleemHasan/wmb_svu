52b71e6fd23124be651c5958b0038c695b77278d
Ali Hasan <ali1saleem1hasan@gmail.com>
2024-03-30 16:14:20 +0300

    connect to docker database using typeorm and pg

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { configService } from './config/configService';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(configService.getTypeOrmConfig())],
   controllers: [AppController],
   providers: [AppService],
 })
